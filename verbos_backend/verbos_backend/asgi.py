fb37e1cd8e7f502ff9fa33864ac7a13f3f54685a
rtiwari13 <ritanshutiwari593@gmail.com>
2025-05-02 23:54:43 +0530

    setup

---
+++ verbos_backend/verbos_backend/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for verbos_backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'verbos_backend.settings')
+
+application = get_asgi_application()
