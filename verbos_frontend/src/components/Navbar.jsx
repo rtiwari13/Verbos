49f89e1a40ff74e58209c047ceb417017aecb39b
rtiwari13 <ritanshutiwari593@gmail.com>
2025-05-17 02:00:28 +0530

    fixed navbar

--- verbos_frontend/src/components/Navbar.jsx
+++ verbos_frontend/src/components/Navbar.jsx
@@ -3,18 +3,18 @@ import AuthPopup from "./AuthPopup";
 
 function Navbar() {
   return (
-    <div className="bg-[var(--background)] text-[var(--text-nav)]  flex items-center justify-between px-10 py-8 ">
+    <div className="bg-[var(--background)] text-[var(--text-nav)] flex items-center justify-between px-10 py-8">
       <div className="text-xl font-bold">
-        <a>Verbos</a>
+        <a className="text-[var(--primary)] hover:text-[var(--ring)] transition-colors duration-200">Verbos</a>
       </div>
 
       <div className=" flex items-center justify-centre gap-10 ">
-        <a href="/docs">Docs</a>
-        <a href="/notes">Notes</a>
-        <a href="/todo">Todos</a>
-        <a href="">Features</a>
-        <a href="">Services</a>
-        <a href="">Login</a>
+        <a href="/docs" className="hover:text-[var(--primary)] transition-colors duration-200">Docs</a>
+        <a href="/notes" className="hover:text-[var(--primary)] transition-colors duration-200">Notes</a>
+        <a href="/todo" className="hover:text-[var(--primary)] transition-colors duration-200">Todos</a>
+        <a href="" className="hover:text-[var(--primary)] transition-colors duration-200">Features</a>
+        <a href="" className="hover:text-[var(--primary)] transition-colors duration-200">Services</a>
+        <a href="" className="hover:text-[var(--primary)] transition-colors duration-200">Login</a>
       </div>
 
       <div className="">
