8670cf3cd4adc837996241580f66b9863824a022
rtiwari13 <ritanshutiwari593@gmail.com>
2025-10-19 21:21:06 +0530

    setup cors mdware

--- verbos_backend/app/main.py
+++ verbos_backend/app/main.py
@@ -1,10 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI 
+from fastapi.middleware.cors import CORSMiddleware
 from app.users.router import auth_router, user_router
 from app.notebook.router import notebook_page_router, notebook_router
 from app.document.router import doc_router
 from app.task_manager.router import board_router , task_router , card_router , tag_router
 
 app = FastAPI()
+
+origins = ["http://localhost:3000"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # or ["*"] to allow all
+    allow_credentials=True,
+    allow_methods=["*"],              # allow all HTTP methods
+    allow_headers=["*"],              # allow all headers
+)
 
 @app.get("/")
 async def root():
